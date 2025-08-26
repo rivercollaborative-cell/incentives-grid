c7700961870832899ba18e1f1f8c45eb39fde9db
rivercollaborative-cell <rivercollaborative@gmail.com>
2025-08-25 21:54:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import IncentivesGrid from './IncentivesGrid';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-export default function App() {
-  return <IncentivesGrid />;
-}
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
